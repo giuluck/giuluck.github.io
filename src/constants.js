7beaec2f790a9e372f757eb24c214870e520e66b
giuluck <giuluck9@gmail.com>
2025-07-11 02:24:40 +0200

    Implement support for italian and english language. Fix other visualization issues in the header.

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-const age = new Date(Date.now() - new Date(1997, 8, 15).getTime()).getFullYear() - 1970
-
-const info = {
-    "IT": {gender: "Lui", age: age + " Anni", location: "Bologna (BO), 40129"},
-    "EN": {gender: "He/Him", age: age + " Years Old", location: "Bologna, Italy (IT)"}
-}
--- src/controller.js
+++ src/controller.js
@@ -4,10 +4,23 @@ angular.module("module", ["ngRoute"])
         $locationProvider.html5Mode(false)
         $locationProvider.hashPrefix("")
         $routeProvider
-            .when("/home", {templateUrl: "src/templates/home.html"})
-            .otherwise({redirectTo: "/home"})
+            .when("/about", {templateUrl: "src/templates/about.html", reloadOnUrl: false})
+            .when("/education", {templateUrl: "src/templates/education.html", reloadOnUrl: false})
+            .otherwise({redirectTo: "/about"})
     })
 
-    .controller("controller", function ($scope) {
-        $scope.info = info["IT"]
+    .controller("controller", function ($scope, $location) {
+        const language = $location.search().lang
+        $scope.language = language === undefined ? "it" : language
+        $scope.age = new Date(Date.now() - new Date(1997, 8, 15).getTime()).getFullYear() - 1970
+
+        $scope.languageSwitch = function () {
+            const language = $scope.language === "it" ? "en" : "it"
+            $location.search("lang", language)
+            $scope.language = language
+        }
+
+        $scope.$on('$locationChangeStart', function () {
+            $location.search("lang", $scope.language)
+        })
     })
