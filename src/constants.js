9d942d7c4bbb1795e310385a4093281e4eefc4b8
giuluck <giuluck9@gmail.com>
2025-07-15 00:46:37 +0200

    Implement projects page and fix style of all pages

--- src/constants.js
+++ src/constants.js
@@ -113,15 +113,20 @@ function build(language) {
             role: select("Research Fellow", "Assegnista di Ricerca"),
             preposition: at,
             institution: unibo,
+            link: `https://www.unibo.it/${language}`,
             period: `11/2024 - ${select("Ongoing", "In Corso")}`,
             description: select({
-                "Funding Project": `<a href="https://prode.unife.it/" target="_blank">PRODE</a>: Probabilistic Declarative Process Mining (Italian RPNI).</li>`,
+                "Funding Project": `<ul>
+                    <li><a href="https://prode.unife.it/" target="_blank">PRODE</a>: Probabilistic Declarative Process Mining (Italian RPNI).</li>
+                </ul>`,
                 "Research Topics": `<ul>
                     <li>Declarative Process Mining with Probabilistic Support.</li>
                     <li>Integration of Causal Knowledge in Declarative Process Mining.</li>
                 </ul>`
             }, {
-                "Progetto Finanziatore": `<a href="https://prode.unife.it/" target="_blank">PRODE</a>: Probabilistic Declarative Process Mining (PRIN).</li>`,
+                "Progetto Finanziatore": `<ul>
+                    <li><a href="https://prode.unife.it/" target="_blank">PRODE</a>: Probabilistic Declarative Process Mining (PRIN).</li>
+                </ul>`,
                 "Temi di Ricerca": `<ul>
                     <li>Process Mining Dichiarativo con Supporto Probabilistico.</li>
                     <li>Integrazione di Conoscenza Causale nel Process Mining Dichiarativo.</li>
@@ -131,6 +136,7 @@ function build(language) {
             role: select("Teaching Assistant", "Tutor Didattico"),
             preposition: at,
             institution: unibo,
+            link: `https://www.unibo.it/${language}`,
             period: `09/2021 - ${select("Ongoing", "In Corso")}`,
             description: select({
                 '<a href="https://www.unibo.it/en/study/course-units-transferable-skills-moocs/course-unit-catalogue/course-unit/2024/446609" target="_blank">Artificial Intelligence in Industry</a>': `2021/22, 2022/23, 2023/24, 2024/25.`,
@@ -147,6 +153,7 @@ function build(language) {
             role: select("PhD", "Dottorato"),
             preposition: "in",
             institution: "Computer Science & Engineering",
+            link: `https://phd.unibo.it/cse/${language}`,
             period: "11/2021 - 04/2025",
             description: select({
                 Institution: `University of Bologna.`,
@@ -187,6 +194,7 @@ function build(language) {
             role: select("Master Degree", "Laurea Magistrale"),
             preposition: "in",
             institution: "Artificial Intelligence",
+            link: `https://corsi.unibo.it/${select("2cycle", "magistrale")}/artificial-intelligence`,
             mark: `110/110 ${select("with Honors", "con Lode")}`,
             period: "09/2019 - 07/2021",
             description: select({
@@ -212,6 +220,7 @@ function build(language) {
             role: select("Academic Internship", "Tirocinio Accademico"),
             preposition: at,
             institution: unibo,
+            link: `https://www.unibo.it/${language}`,
             period: "02/2019 - 05/2019",
             description: select({
                 Institution: `University of Bologna, Cesena Campus.`,
@@ -232,6 +241,7 @@ function build(language) {
             role: select("Bachelor Degree", "Laurea"),
             preposition: "in",
             institution: select("Computer Science & Engineering", "Ingegneria e Scienze Informatiche"),
+            link: `https://corsi.unibo.it/${select("1cycle/ComputerScienceEngineering", "laurea/IngegneriaScienzeInformatiche")}`,
             mark: `110/110 ${select("with Honors", "con Lode")}`,
             period: "09/2016 - 10/2019",
             description: select({
@@ -255,6 +265,7 @@ function build(language) {
             role: select("Professional Stage", "Stage Professionale"),
             preposition: at,
             institution: select("Loccioni Group", "Gruppo Loccioni"),
+            link: `https://www.loccioni.com/${language}`,
             period: "06/2015 - 07/2015",
             description: select({
                 Company: `Loccioni Group, Angeli di Rosora (IT).`,
@@ -297,6 +308,14 @@ function build(language) {
             link: `https://www.ijcai.org/proceedings/2023/644`,
             abstract: `Artificial Intelligence (AI) has gradually attracted attention in the field of artistic creation, resulting in a debate on the evaluation of AI artistic outputs. However, there is a lack of common criteria for objective artistic evaluation both of human and AI creations. This is a frequent issue in the field of dance, where different performance metrics focus either on evaluating human or computational skills separately. This work proposes a methodological approach for the artistic evaluation of both AI and human artistic creations in the field of robotic dance. First, we define a series of common initial constraints to create robotic dance choreographies in a balanced initial setting, in collaboration with a group of human dancers and choreographer. Then, we compare both creation processes through a human audience evaluation. Finally, we investigate which choreography aspects (e.g., the music genre) have the largest impact on the evaluation, and we provide useful guidelines and future research directions for the analysis of interconnections between AI and human dance creation.`
         }, {
+            title: `Achieving Intersectional Algorithmic Fairness By Constructing A Maximal Correlation Latent Space`,
+            authors: `<u>L. Giuliani</u>, M. Lombardi`,
+            publisher: `${select(
+                "Accepted (Not Published Yet) at",
+                "Accettato (Non Ancora Pubblicato) a"
+            )} ECAI, 2025 (A Conference)`,
+            abstract: `Recent developments in algorithmic fairness started to investigate the interaction between multiple sensitive information through an intersectional perspective. We introduce a new definition of intersectional fairness based on a multivariate extension of the Generalized Disparate Impact (GeDI). Our approach leverages a neural network to transform multiple protected groups into a univariate latent space that maximizes correlation with the target, effectively capturing unfairness across all potential subgroups even with limited data samples. Empirical evaluations on several benchmarks demonstrate that our method can be effectively used as a loss regularizer during neural network training, offering stronger performance guarantees compared to existing intersectional statistical parity definitions while also allowing to manage continuous inputs and targets.`
+        }, {
             title: `MusiComb: a Sample-based Approach to Music Generation Through Constraints`,
             authors: `<u>L. Giuliani</u>, F. Ballerini, A. De Filippo, A. Borghesi`,
             publisher: `ICTAI, 2023 (B Conference)`,
@@ -315,6 +334,14 @@ function build(language) {
             link: `https://ceur-ws.org/Vol-3810/paper4.pdf`,
             abstract: `The significant expansion of data-driven technologies in the past decade has highlighted the crucial role of structured data, given the more relevant and meaningful informative content that they can provide to artificial intelligence (AI) applications. Nonetheless, there are domains based on inherently unstructured data, such as the audio domain. In those cases, the possibility of relying on an automated system capable of extracting structured features from raw data could serve as a pivotal element in enhancing and strengthening the capabilities of an AI system. In this work, we propose an automated feature extractor which leverages machine and deep learning methodologies to retrieve two higher-level musical attributes from short MIDI samples, namely the harmonic content of the sample – through its chords progression – and the role that such sample could have within a multi-track composition – i.e., melody, bass, or accompaniment. We perform our tests on a dataset containing ground truth information to assess quantitative results and later integrate our models within the state-of-the-art framework for combinatorial music generation MusiComb to check for harmonic and melodic consonance on the downstream generative task.`
         }, {
+            title: `Beyond Temporal Relationships: Causal Support in Declarative Process Modeling`,
+            authors: `<u>L. Giuliani</u>, A. Zecchini`,
+            publisher: `${select(
+                "Accepted (Not Published Yet) at",
+                "Accettato (Non Ancora Pubblicato) a"
+            )} PMAI Workshop @ ECAI, 2025 (A Conference)`,
+            abstract: `Process discovery algorithms extract knowledge about processes by analyzing temporal relationships only, often disregarding any additional data available in the log. We propose a novel approach that leverages causal discovery to detect the underlying relationships between data features and events. The acquired knowledge is then used to complement an existing declarative process model by measuring the causal support between pairs of events, potentially allowing to remove inconsistent constraints to enhance robustness and clarity of the model. We discuss the details of this approach using an example on synthetic data to show its advantages and limitations.`
+        }, {
             title: `A Geometric Framework for Fairness`,
             authors: `A. Maggio, <u>L. Giuliani</u>, R. Calegari, M. Lombardi, M. Milano`,
             publisher: `AEQUITAS Workshop @ ECAI, 2023 (A Conference)`,
@@ -332,6 +359,106 @@ function build(language) {
             publisher: `CREAI Workshop @ AIxIA, 2022 (C Conference)`,
             link: `https://ceur-ws.org/Vol-3278/short3.pdf`,
             abstract: `In recent years, the interplay between creativity and Artificial Intelligence (AI) has been gaining more and more attention from the research community. Within this vast area, humanoid robots have been successfully used in artistic research areas, and many works have studied and implemented systems for robotic dance. However, only few works take into account the human evaluation of these artistic outputs. For this aim, we start from a recent work focused on defining criteria for the evaluation of robotic dance performances, and we analyze a further crucial aspect such as the need for a multi-modal perspective: the musical element needs to blend seamlessly and organically with the choreography, in accordance to the judgments expressed by human evaluators. Based on the analysis of results, musical elements emerged as having a large impact on the artistic evaluation. For this reason, the final purpose of this work is twofold: we would like both to explore new creative paths where humans ideas can be broadened by the use of AI software, but also to help both human choreographers and AI algorithms to create dance performances and music with a major impact on the audience.`
+        }],
+        // PROJECTS DATA
+        projects: [{
+            title: `MaxCorr`,
+            link: `https://github.com/giuluck/maxcorr`,
+            tagline: select(
+                `A Python Package for the Estimation of Maximal Correlation Indicators`,
+                `Un Pacchetto Python per il Calcolo di Indicatori di Correlazione Massima`
+            )
+        }, {
+            title: `Moving Targets`,
+            link: `https://github.com/moving-targets/moving-targets`,
+            tagline: select(
+                `A Python Package for Constrained Machine Learning based on Bi-level Decomposition`,
+                `Un Pacchetto Python per il Machine Learning Vincolato basato su una Decomposizione Bi-livello`
+            )
+        }, {
+            title: `Causalgen`,
+            link: `https://github.com/giuluck/causalgen`,
+            tagline: select(
+                `A Python Package for Data Generation based on Causal Dependencies`,
+                `Un Pacchetto Python per la Generazione di Dati basata su Dipendenze Causali`
+            )
+        }, {
+            title: `Non-Linear Correlations`,
+            link: `https://github.com/giuluck/non-linear-correlations`,
+            tagline: select(
+                `Experiments to Reproduce the Results showed in my PhD Dissertation`,
+                `Esperimenti per Riprodurre tutti i Risultati mostrati nella mia Tesi di Dottorato`
+            )
+        }, {
+            title: `Another Genetic Circuit Transcriber`,
+            link: `https://github.com/AGCT-DSL/AGCT`,
+            tagline: select(
+                `A Kotlin-based Domain-Specific Language for the Definition of Gene Regulatory Networks`,
+                `Un Linguaggio Dominio-Specifico basato su Kotlin per la Definizione di Reti Geniche`
+            )
+        }, {
+            title: `Powerplantsim`,
+            link: `https://github.com/giuluck/powerplantsim`,
+            tagline: select(
+                `A Graph-based Simulator for Power Plants`,
+                `Un Simulatore di Power Plants basato su Grafi`
+            )
+        }, {
+            title: `Interactive Benchmark Library`,
+            link: `https://github.com/giuluck/benchmark-library`,
+            tagline: select(
+                `A Benchmark Library developed for the TUPLES European Project`,
+                `Una Libreria di Benchmark sviluppata per il Progetto Europeo TUPLES`
+            )
+        }, {
+            title: `Epidemic Model Learning`,
+            link: `https://github.com/mazzio97/EpidemicModelLearning`,
+            tagline: select(
+                `An Analysis of Covid Data based on Machine Learning and Mathematical Programming`,
+                `Un'Analisi di Dati Covid basata su Machine Learning e Programmazione Matematica`
+            )
+        }, {
+            title: `Deep Comedy`,
+            link: `https://github.com/mazzio97/DeepComedy`,
+            tagline: select(
+                `A Transformer-based Neural Architecture aimed at reproducing the Style of the Divine Comedy`,
+                `Un'Architettura Neural basata su Transformer per riprodurre lo Stile della Divina Commedia`
+            )
+        }, {
+            title: `Gangster SQuAD`,
+            link: `https://github.com/giuluck/Gangster-SQuAD`,
+            tagline: select(
+                `A BERT-based Natural Language Processing System for Question Answering Tasks`,
+                `Un Sistema di Elaborazione del Linguaggio Naturale basato su BERT per il Problema di Question Answering`
+            )
+        }, {
+            title: `IoTrace`,
+            link: `https://github.com/mazzio97/IoTrace`,
+            tagline: select(
+                `A Prototype of Contact Tracing App built with IOTA`,
+                `Un Prototipo di App per il Contact Tracing sviluppata con IOTA`
+            )
+        }, {
+            title: `Paku Paku`,
+            link: `https://github.com/giuluck/PakuPaku`,
+            tagline: select(
+                `A One-Vs-One Pacman developed in C++ using OpenGL Primitives`,
+                `Un Pacman Uno-Contro-Uno sviluppato in C++ usando Primitive OpenGL`
+            )
+        }, {
+            title: `Snailysis`,
+            link: `https://github.com/mazzio97/Snailysis`,
+            tagline: select(
+                `A Level-based Platform Game with a Weird Focus on Calculus`,
+                `Un Platform Game a Livelli con uno Strano Focus sull'Analisi Matematica`
+            )
+        }, {
+            title: `AlmaFood`,
+            link: `https://github.com/mazzio97/AlmaFood`,
+            tagline: select(
+                `A Prototype of a JustEat Clone for the Cesena Campus of the University of Bologna`,
+                `Un Prototipo di Clone di JustEat per il Campus di Cesena dell'Università di Bologna`
+            )
         }]
     }
 }
